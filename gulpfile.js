b6c61b302556e6538fffa8381a23e3ddda6ee427
krzosdominik <krzosdominik@gmail.com>
2020-01-03 10:59:19 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+sass.compiler = require('node-sass');
+
+const server = function(cb) {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    cb();
+}
+
+const css = function() {
+    return gulp.src('./scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle : "expanded"
+        }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+}
+
+const watch = function(cb) {
+    gulp.watch("./scss/**/*.scss", gulp.series(css));
+    gulp.watch("./*.html").on('change', browserSync.reload);
+    cb();
+}
+
+exports.css = css;
+exports.watch = watch;
+exports.default = gulp.series(css, server, watch);
